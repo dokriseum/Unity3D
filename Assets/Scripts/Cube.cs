b48de1636cafdc7b0c257ba3b7d0b0ae47e0fca6
Dokriseum <eiki300499@googlemail.com>
2024-12-27 23:45:51 +0100

    Init Tutorial

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
