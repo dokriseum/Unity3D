dcc6cec8cf3a0f9cfcaa57ad6a4e834a0de12048
Dokriseum <eiki300499@googlemail.com>
2024-12-28 00:28:32 +0100

    erste Bausteine mit Materialänderung implementiert

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Cube : MonoBehaviour
 {
+    public List<Material> materials; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,17 +14,34 @@ public class Cube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
-            GetComponent<Renderer>().material.color = Color.red;
+            GetComponent<Renderer>().material = this.materials.Find(m => m.name == "Red");
         }
         if (Input.GetKeyDown(KeyCode.G))
         {
-            GetComponent<Renderer>().material.color = Color.green;
+            GetComponent<Renderer>().material = this.materials.Find(m => m.name == "Green");
         }
         if (Input.GetKeyDown(KeyCode.B))
         {
-            GetComponent<Renderer>().material.color = Color.blue;
+            GetComponent<Renderer>().material = this.materials.Find(m => m.name == "Blue");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Renderer>().material = this.materials.Find(m => m.name == "Wood");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            GetComponent<Renderer>().material = this.materials.Find(m => m.name == "Conquit");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GetComponent<Renderer>().material = this.materials.Find(m => m.name == "Stone");
         }
     }
 }
